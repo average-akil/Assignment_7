3d7d8219d2e5fed7797b05184165ef6a9f0bbe4e
average-akil <ahsoykat22@gmail.com>
2025-11-18 05:29:20 +0600

    10th

--- src/assets/Selected_Players/Selected_players.jsx
+++ src/assets/Selected_Players/Selected_players.jsx
@@ -4,9 +4,7 @@ import SelectedPlayer from './SelectedPlayer';
 const Selected_players = ({ selectedPlayers = [], removeSelected }) => {
   return (
     <div className=" flex flex-col ">
-      <h1 className="text-lg font-semibold mb-3">
-        Selected Players: {selectedPlayers.length}
-      </h1>
+    
       <div className="flex flex-col gap-4">
         {selectedPlayers.map((p) => (
           <SelectedPlayer key={p.id} player={p} removeSelected={removeSelected} />
